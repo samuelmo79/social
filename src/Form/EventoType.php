2a2a51536812c0247836beed8f7a1d991bf33c1d
Samuel <samuelmo@pmsg.rj.gov.br>
2020-09-23 14:04:56 -0300

    feat: Melhorias no layout

--- src/Form/EventoType.php
+++ src/Form/EventoType.php
@@ -8,6 +8,7 @@ use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,28 +17,42 @@ class EventoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titulo')
+            ->add('titulo', TextType::class, [
+                'label' => 'Título *'
+            ])
             ->add('descricao', CKEditorType::class, [
-                'label' => 'Conteúdo',
+                'label' => 'Conteúdo *',
                 'config' => [
                     'toolbar' => 'ferramentas',
                     'required' => true,
                 ]
             ])
             ->add('dataEvento', DateType::class, [
-                'label' => 'Data/Hora do Evento',
+                'label' => 'Data/Hora do Evento *',
                 'widget' => 'single_text'
             ])
             ->add('tipoEvento', ChoiceType::class, [
-                'label' => 'Tipo de Evento',
+                'label' => 'Tipo de Evento *',
                 'choices' => array_flip(PrivacidadeEnum::getPrivacidade())
             ])
-            ->add('rua')
-            ->add('numero')
-            ->add('complemento')
-            ->add('bairro')
-            ->add('estado')
-            ->add('cep')
+            ->add('rua', TextType::class, [
+                'label' => 'Rua *'
+            ])
+            ->add('numero', TextType::class, [
+                'label' => 'Número *'
+            ])
+            ->add('complemento', TextType::class, [
+                'label' => 'Complemento'
+            ])
+            ->add('bairro', TextType::class, [
+                'label' => 'Bairro *'
+            ])
+            ->add('estado', TextType::class, [
+                'label' => 'Estado/Provincia *'
+            ])
+            ->add('cep', TextType::class, [
+                'label' => 'CEP *'
+            ])
             ->add('imagem')
         ;
     }
