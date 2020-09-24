fb03d2b6d25d8876e97b50af509da98875f8d39d
Samuel <samuelmo@pmsg.rj.gov.br>
2020-09-24 14:44:14 -0300

    feat: Botão submit

--- src/Form/PostComentarioType.php
+++ src/Form/PostComentarioType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\PostComentario;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,6 +14,9 @@ class PostComentarioType extends AbstractType
     {
         $builder
             ->add('comentario')
+            ->add('enviar', SubmitType::class, [
+                'label' => 'Enviar'
+            ])
         ;
     }
 
