13f9f8d7f0d214a4c12fbc4c378b4b0816194017
Samuel <samuelmo@pmsg.rj.gov.br>
2020-09-25 13:16:07 -0300

    feat: Upload de imagem para evento

--- src/Form/EventoType.php
+++ src/Form/EventoType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class EventoType extends AbstractType
 {
@@ -53,7 +54,13 @@ class EventoType extends AbstractType
             ->add('cep', TextType::class, [
                 'label' => 'CEP *'
             ])
-            ->add('imagem')
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Foto do perfil',
+                'required' => false,
+                'allow_delete' => false,
+                'download_label' => '',
+                'image_uri' => false,
+            ])
         ;
     }
 
