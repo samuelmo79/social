989a868a516b61d30d690e0a6e426562d1cd5219
rogerbernar9 <roger.bernar9@gmail.com>
2020-09-30 14:16:57 -0300

    Cadastra localizacao no formulario de registro

--- src/Form/LocalizacaoType.php
+++ src/Form/LocalizacaoType.php
@@ -4,18 +4,36 @@ namespace App\Form;
 
 use App\Entity\Localizacao;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class LocalizacaoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('bairro')
-            ->add('cidade')
-            ->add('estado')
-            ->add('pais')
+            ->add('bairro', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Este campo é obrigatório']),
+                ],
+            ])
+            ->add('cidade', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Este campo é obrigatório']),
+                ],
+            ])
+            ->add('estado', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Este campo é obrigatório']),
+                ],
+            ])
+            ->add('pais', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Este campo é obrigatório']),
+                ],
+            ])
         ;
     }
 
