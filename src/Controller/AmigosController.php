57c622832d6d129ae6d03728c69ac3d7114e1f6c
Samuel <samuelmo@pmsg.rj.gov.br>
2020-10-08 10:43:49 -0300

    feat:  Paginação

--- src/Controller/AmigosController.php
+++ src/Controller/AmigosController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,12 +23,15 @@ class AmigosController extends AbstractController
      * @Route("/amigos", name="amigos", methods={"GET"})
      * @Template("amigos/index.html.twig")
      */
-    public function index(Request $request)
+    public function index(Request $request, PaginatorInterface $paginator)
     {
         $pesquisarAmigos = $request->get('pesquisar_amigos');
 
         $user = $this->em->getRepository(User::class)
             ->findByAmigos($pesquisarAmigos, $this->getUser());
+
+        $page = $request->query->getInt('page', 1);
+        $user = $paginator->paginate($user, $page, 20);
 
         return [
             'user' => $user,
