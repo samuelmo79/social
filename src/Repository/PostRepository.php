2cc033cf634716ce83c755726d9a014b26825b96
rogerbernar9 <roger.bernar9@gmail.com>
2020-11-04 16:04:09 -0300

    feat: criação da query que lista postagens de amigos publicas ou por amigos

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Post;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use function Doctrine\ORM\QueryBuilder;
 
 /**
  * @method Post|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +18,32 @@ class PostRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Post::class);
+    }
+
+    public function findPostagemAmigosPrivados($id)
+    {
+        $subquery = $this->getEntityManager()->createQueryBuilder();
+        $subquery = $subquery->select('u.id')
+            ->from('App:User', 'u')
+            ->from('App:Amizade', 'a')
+            ->andWhere('u.id = a.amigo')
+            ->andWhere('a.usuario = :uid')
+            ->setParameter('uid', $id);
+
+        $q = $this->createQueryBuilder('p')
+            ->join('App:User','us')
+        ;
+        $q->andWhere('p.privacidade like :amigos');
+        $q->orWhere('p.privacidade like :publico');
+        $q->andWhere(
+            $q->expr()->in('p.autor', $subquery->getDQL())
+        );
+        $q->setParameter('uid',$id);
+        $q->setParameter('amigos', 'Amigos');
+        $q->setParameter('publico', 'Publico');
+
+
+        return $q->getQuery()->getResult();
     }
 
     // /**
